20520212af666f90b1d7dbcf4cbf37cfdde802b9
Aleksey Pastuhov <aleksey.pastuhov@gmail.com>
2023-09-13 08:49:53 +0100

    Simplify e2e testing

--- packages/rest/example/e2e.test.ts
+++ packages/rest/example/e2e.test.ts
@@ -1,77 +1,105 @@
 import { describe, test, expect, beforeAll, afterAll } from "bun:test";
 
+const HOST = "http://localhost:3000";
+
 describe("CRUD Users under Organization", () => {
-  const root = "http://localhost:3000";
   let organizationId: number;
   let userId: number;
 
-  const apiRequest = async (method: string, endpoint: string, data?: any) => {
-    const req = new Request({
-      url: root + endpoint,
-      method: method.toUpperCase(),
-      body: data ? JSON.stringify(data) : undefined,
-    });
-
-    const response = await fetch(req);
-    const json = await response.json();
-    return { response, json };
-  };
-
   beforeAll(async () => {
     const organizationData = { name: "Test Organization" };
-    const { json } = await apiRequest(
-      "post",
-      "/organizations",
-      organizationData
-    );
-    organizationId = json.id;
+    const response = await req("post", "/organizations", organizationData);
+    organizationId = (await response.json()).id;
   });
 
   afterAll(async () => {
-    await apiRequest("delete", `/organizations/${organizationId}`);
-  });
-
-  test("qwe", () => {
-    expect(1).toBe(1);
+    await req("delete", `/organizations/${organizationId}`);
   });
 
   test("CREATE User", async () => {
     const data = { name: "Test User", organizationId };
-    const { response, json } = await apiRequest("post", "/users", data);
-    expect(response.status).toBe(201);
-    expect(json).toMatchObject(data);
-    userId = json.id;
+    const response = await req("post", "/users", data);
+    userId = (await response.clone().json()).id;
+    await response.toMatchObject(data);
+    response.hasStatus(201);
   });
 
   test("READ User", async () => {
-    const { json } = await apiRequest("get", `/users/${userId}`);
-    expect(json.id).toBe(userId);
+    const response = await req("get", `/users/${userId}`);
+    await response.toMatchObject({ id: userId });
   });
 
   test("READ All Users under Organization", async () => {
-    const { json } = await apiRequest(
-      "get",
-      `/organizations/${organizationId}/users`
-    );
-    expect(Array.isArray(json)).toBe(true);
-    expect(json).toMatchObject([
+    const response = await req("get", `/organizations/${organizationId}/users`);
+    await response.toMatchObject([
       {
         id: userId,
         name: "Test User",
         organizationId: 1,
       },
     ]);
-    // expect(json).toEqual(expect.arrayContaining([expect.objectContaining({ id: userId })]));
   });
 
   test("UPDATE User", async () => {
     const data = { name: "Updated User", organizationId };
-    const { json } = await apiRequest("put", `/users/${userId}`, data);
-    expect(json).toEqual(data);
+    const response = await req("put", `/users/${userId}`, data);
+    response.isOk();
+    await response.toEqual(data);
   });
 
   test("DELETE User", async () => {
-    const { response } = await apiRequest("delete", `/users/${userId}`);
-    expect(response.status).toBe(200);
+    const response = await req("delete", `/users/${userId}`);
+    response.isOk();
+    await response.toBe("User deleted");
   });
 });
+
+/**
+ * Helper methods for e2e testing
+ * TODO: move to library/package
+ */
+
+async function req(method: string, endpoint: string, data?: any) {
+  const req = new Request({
+    url: HOST + endpoint,
+    method: method.toUpperCase(),
+    body: data ? JSON.stringify(data) : undefined,
+  });
+
+  const response = await fetch(req);
+
+  return response;
+}
+
+declare global {
+  interface Response {
+    hasStatus(status: number): void;
+    isOk(): void;
+    toBe(text: string): Promise<void>;
+    toMatchObject(obj: any): Promise<void>;
+    toEqual(obj: any): Promise<void>;
+  }
+}
+
+Response.prototype.toBe = async function (text: string) {
+  const actualText = await this.text();
+  expect(actualText).toBe(text);
+};
+
+Response.prototype.toMatchObject = async function (obj: any) {
+  const actualJson = await this.json();
+  expect(actualJson).toMatchObject(obj);
+};
+
+Response.prototype.toEqual = async function (obj: any) {
+  const actualJson = await this.json();
+  expect(actualJson).toEqual(obj);
+};
+
+Response.prototype.hasStatus = function (status: number) {
+  expect(this.status).toBe(status);
+};
+
+Response.prototype.isOk = function () {
+  expect(this.status).toBe(200);
+};
